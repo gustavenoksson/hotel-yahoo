6c4c6e3922c8493bb199c2babe6c6259e2174f54
jonas128 <jonasm@outlook.com>
2023-01-06 19:43:18 +0100

    prices and light button update

--- index.php
+++ index.php
@@ -6,111 +6,111 @@ require __DIR__ . '/header.php';
 ?>
 
 <header>
-    <?php require __DIR__ . '/date.php'; ?>
+          <?php require __DIR__ . '/date.php'; ?>
 </header>
 
 <main>
-    <div class="history">
-        <img width="700" height="200" alt="history" src="images/pelago.jpeg">
-        <button id="light">
-            Lights out button!
-        </button>
-    </div>
+          <div class="history">
+                    <img width="700" height="200" alt="history" src="images/pelago.jpeg">
+                    <button id="light">
+                              +light switch button+
+                    </button>
+          </div>
 
-    <p>Yahoos are legendary beings in the 1726 satirical novel Gulliver's Travels written by Jonathan Swift. The word "yahoo" was coined by Jonathan Swift in the fourth section of Gulliver's Travels and has since entered the English language more broadly.</p>
+          <p>Yahoos are legendary beings in the 1726 satirical novel Gulliver's Travels written by Jonathan Swift. The word "yahoo" was coined by Jonathan Swift in the fourth section of Gulliver's Travels and has since entered the English language more broadly.</p>
 
-    <div class="calendar">
-        <h3>Cheap room Close to Nature (4$) -></h3>
-        <!-- <button id="newroom">
+          <div class="calendar">
+                    <h3>Cheap room Close to Nature (4$) -></h3>
+                    <!-- <button id="newroom">
                               Change room
                     </button> -->
-        <img width="350" height="350" alt="room1" src="images/room1.jpeg">
-        <table cellspacing="0">
-            <tbody>
-                <tr class="weekdays">
-                    <th>@Mon</th>
-                    <th>@Tue</th>
-                    <th>@Wed</th>
-                    <th>@Thu</th>
-                    <th>@Fri</th>
-                    <th>@Sat</th>
-                    <th>@Sun</th>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>1</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>3</td>
-                    <td>4</td>
-                    <td>5</td>
-                    <td>6</td>
-                    <td>7</td>
-                    <td>8</td>
-                </tr>
-                <tr>
-                    <td>9</td>
-                    <td>10</td>
-                    <td>11</td>
-                    <td>12</td>
-                    <td>13</td>
-                    <td>14</td>
-                    <td>15</td>
-                </tr>
-                <tr>
-                    <td>16</td>
-                    <td>17</td>
-                    <td>18</td>
-                    <td>19</td>
-                    <td>20</td>
-                    <td>21</td>
-                    <td>22</td>
-                </tr>
-                <tr>
-                    <td>23</td>
-                    <td>24</td>
-                    <td>25</td>
-                    <td>26</td>
-                    <td>27</td>
-                    <td>28</td>
-                    <td>29</td>
-                </tr>
-                <tr>
-                    <td>30</td>
-                    <td>31</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="change">
-        <button id="newroom">
-            CHANGE ROOM
-        </button>
-    </div>
+                    <img width="350" height="350" alt="room1" src="images/room1.jpeg">
+                    <table cellspacing="0">
+                              <tbody>
+                                        <tr class="weekdays">
+                                                  <th>@Mon</th>
+                                                  <th>@Tue</th>
+                                                  <th>@Wed</th>
+                                                  <th>@Thu</th>
+                                                  <th>@Fri</th>
+                                                  <th>@Sat</th>
+                                                  <th>@Sun</th>
+                                        </tr>
+                                        <tr>
+                                                  <td></td>
+                                                  <td></td>
+                                                  <td></td>
+                                                  <td></td>
+                                                  <td></td>
+                                                  <td></td>
+                                                  <td>1</td>
+                                        </tr>
+                                        <tr>
+                                                  <td>2</td>
+                                                  <td>3</td>
+                                                  <td>4</td>
+                                                  <td>5</td>
+                                                  <td>6</td>
+                                                  <td>7</td>
+                                                  <td>8</td>
+                                        </tr>
+                                        <tr>
+                                                  <td>9</td>
+                                                  <td>10</td>
+                                                  <td>11</td>
+                                                  <td>12</td>
+                                                  <td>13</td>
+                                                  <td>14</td>
+                                                  <td>15</td>
+                                        </tr>
+                                        <tr>
+                                                  <td>16</td>
+                                                  <td>17</td>
+                                                  <td>18</td>
+                                                  <td>19</td>
+                                                  <td>20</td>
+                                                  <td>21</td>
+                                                  <td>22</td>
+                                        </tr>
+                                        <tr>
+                                                  <td>23</td>
+                                                  <td>24</td>
+                                                  <td>25</td>
+                                                  <td>26</td>
+                                                  <td>27</td>
+                                                  <td>28</td>
+                                                  <td>29</td>
+                                        </tr>
+                                        <tr>
+                                                  <td>30</td>
+                                                  <td>31</td>
+                                                  <td></td>
+                                                  <td></td>
+                                                  <td></td>
+                                                  <td></td>
+                                                  <td></td>
+                                        </tr>
+                              </tbody>
+                    </table>
+          </div>
+          <div class="change">
+                    <button id="newroom">
+                              CHANGE ROOM
+                    </button>
+          </div>
 
-    <!-- <button id="newroom">
+          <!-- <button id="newroom">
                     Change room
           </button> -->
 
-    <form action="./booking.php" method="POST">
+          <form action="./booking.php" method="POST">
 
-        <p>HOTEL YAHOO BOOKING FORM</p>
+                    <p>HOTEL YAHOO BOOKING FORM</p>
 
-        <label for="name">Name</label>
-        <input class="name" type="text" name="name">
+                    <label for="name">Name</label>
+                    <input class="name" type="text" name="name">
 
-        <!-- <label for="room">Room</label>
+                    <!-- <label for="room">Room</label>
                     <select name="room" class="choose-room">
                               <option value="1" id="budget">RoomI</option>
                               <option value="2" id="standard">RoomII</option>
@@ -119,42 +119,42 @@ require __DIR__ . '/header.php';
 
 
 
-        <label for="arrival">Arrival</label>
-        <input class="date" type="date" name="arrival" min="2023-01-01" max="2023-01-31" id="arrival">
+                    <label for="arrival">Arrival</label>
+                    <input class="date" type="date" name="arrival" min="2023-01-01" max="2023-01-31" id="arrival">
 
-        <label for="departure">Departure</label>
-        <input class="date" type="date" name="departure" min="2023-01-01" max="2023-01-31" id="departure">
+                    <label for="departure">Departure</label>
+                    <input class="date" type="date" name="departure" min="2023-01-01" max="2023-01-31" id="departure">
 
-        <p>Extras:</p>
+                    <p>Extras:</p>
 
-        <div class="features">
-            <input type="checkbox" name="features[]" value="4" id="food">
-            <label for="features">A Gooh dish (2$)</label>
-            <!-- <p>2$</p> -->
+                    <div class="features">
+                              <input type="checkbox" name="features[]" value="4" id="food">
+                              <label for="features">A Gooh dish (2$)</label>
+                              <!-- <p>2$</p> -->
 
-            <input type="checkbox" name="features[]" value="5" id="email">
-            <label for="features">Yahoo e@mail service (1$)</label>
-            <!-- <p>1$</p> -->
+                              <input type="checkbox" name="features[]" value="5" id="email">
+                              <label for="features">Yahoo e@mail service (1$)</label>
+                              <!-- <p>1$</p> -->
 
-            <input type="checkbox" name="features[]" value="6" id="therapy">
-            <label for="features">API therapist (3$)</label>
-            <!-- <p>3$</p> -->
-        </div>
+                              <input type="checkbox" name="features[]" value="6" id="therapy">
+                              <label for="features">API therapist (3$)</label>
+                              <!-- <p>3$</p> -->
+                    </div>
 
-        <p>Please choose room & optional extras, enter your transfer code and click the book button!</p>
+                    <p>Please choose room & optional extras, enter your transfer code and click the book button!</p>
 
-        <!-- <div class="total-cost">
+                    <!-- <div class="total-cost">
             <h3>Total cost: </h3>
             <p>0 $</p>
         </div> -->
 
-        <label for="transfercode">Transfercode</label>
-        <input class="transfer-code" type="text" name="transfercode">
+                    <label for="transfercode">Transfercode</label>
+                    <input class="transfer-code" type="text" name="transfercode">
 
-        <button class="submit-button" type="submit" name="submit">
-            BOOK A ROOM
-        </button>
-    </form>
+                    <button class="submit-button" type="submit" name="submit">
+                              BOOK A ROOM
+                    </button>
+          </form>
 </main>
 
 
